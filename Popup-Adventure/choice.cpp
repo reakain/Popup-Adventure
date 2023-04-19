c462d1a5c0980e769f772dd272ab293a1abc5a83
Reakain <reakain@users.noreply.github.com>
2023-04-19 08:30:16 -0700

    fixing the choice list, starting making a story I guess

--- Popup-Adventure/choice.cpp
+++ Popup-Adventure/choice.cpp
@@ -22,8 +22,52 @@ void Choice::read(const QJsonObject &json)
         mGoToName = json["goto"].toString();
 
     if (json.contains("type") && json["type"].isString()) {
-        QMetaEnum metaEnum = QMetaEnum::fromType<QMessageBox::StandardButton>();
-        mButton = QMessageBox::StandardButton(metaEnum.keysToValue(json["type"].toString().toStdString().c_str()));
+        //string btnType = json["type"].toString().toStdString();
+        QString btnType = json["type"].toString();
+        if(btnType == "Yes"){
+            mButton = QMessageBox::Yes;
+        } else if(btnType == "No"){
+            mButton = QMessageBox::No;
+        } else if(btnType == "Help"){
+            mButton = QMessageBox::Help;
+        } else if(btnType == "Close"){
+            mButton = QMessageBox::Close;
+        } else if(btnType == "Cancel"){
+            mButton = QMessageBox::Cancel;
+        } else if(btnType == "Ok"){
+            mButton = QMessageBox::Ok;
+        } else if(btnType == "YesToAll"){
+            mButton = QMessageBox::YesToAll;
+        } else if(btnType == "NoToAll"){
+            mButton = QMessageBox::NoToAll;
+        } else if(btnType == "Abort"){
+            mButton = QMessageBox::Abort;
+        } else if(btnType == "Open"){
+            mButton = QMessageBox::Open;
+        } else if(btnType == "Save"){
+            mButton = QMessageBox::Save;
+        } else if(btnType == "Apply"){
+            mButton = QMessageBox::Apply;
+        } else if(btnType == "Reset"){
+            mButton = QMessageBox::Reset;
+        } else if(btnType == "Retry"){
+            mButton = QMessageBox::Retry;
+        } else if(btnType == "Ignore"){
+            mButton = QMessageBox::Ignore;
+        } else if(btnType == "Discard"){
+            mButton = QMessageBox::Discard;
+        } else if(btnType == "SaveAll"){
+            mButton = QMessageBox::SaveAll;
+        } else if(btnType == "RestoreDefaults"){
+            mButton = QMessageBox::RestoreDefaults;
+        }
+            else {
+                mButton = QMessageBox::Ok;
+            }
+
+
+        //QMetaEnum metaEnum = QMetaEnum::fromType<QMessageBox::StandardButton>();
+        //mButton = QMessageBox::StandardButton(metaEnum.keysToValue(json["type"].toString().toStdString().c_str()));
     }
     else mButton = QMessageBox::Ok;
 
